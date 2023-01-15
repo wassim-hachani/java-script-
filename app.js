3c1bb92533d081364ca8e3beace6e4f119693714
wassim-hachani <hachaniwasssim10@gmail.com>
2023-01-15 15:32:25 +0100

    first commit

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log("hello there")
+//console.log("hello there")
+//console.log(1+1)
+
+//----------------------------------------------------------------------------------------------------------------------------------------
+
+//let name ="wassim"
+//let lastname ="Hachani"
+//console.log("my name is :"+ name + " and my lastname is  :"+ lastname)
+
+//----------------------------------------------------------------------------------------------------------------------------------------
+
+//let NAME = prompt("add ur NAME")
+//let LASTNAME = prompt("add ur last name")
+//console.log("my Name is " + NAME + " my LASTNAME is " + LASTNAME )
+
+//----------------------------------------------------------------------------------------------------------------------------------------
+
+let N1 = prompt("add n°1 ")
+let N2 = prompt("add n°2")
+alert(parseInt(N1)+parseInt(N2))
